f8b92b2e36a389272cf99f6ca62eaf6cb810fb60
Carlos Sanchez <carlos.2619@outlook.com>
2021-01-31 21:14:16 -0500

    avance y simulacion de submit e input change

--- src/test/AddCategory.test.js
+++ src/test/AddCategory.test.js
@@ -3,8 +3,16 @@ import { shallow } from 'enzyme';
 import { AddCategory } from '../components/AddCategory';
 
 describe('Pruebas en <AddCategory/>', () => {
-	const setCategories = () => {};
-	const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+	// const setCategories = () => {};
+	// ahora con la ayuda de jest declararemos el mismo callback,
+	// pero ahora podremos simular el comportmiento del mismo.
+	const setCategories = jest.fn();
+	let wrapper = shallow(<AddCategory setCategories={setCategories} />);
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+		wrapper = shallow(<AddCategory setCategories={setCategories} />);
+	});
 
 	test('Debe de tomar el snapshot y funcionar correctamente', () => {
 		expect(wrapper).toMatchSnapshot();
@@ -21,5 +29,31 @@ describe('Pruebas en <AddCategory/>', () => {
 			},
 		});
 		expect(wrapper.find('p').text().trim()).toBe(value);
+	});
+
+	test('No debe postear la informacion con submmit', () => {
+		// de la siguiente manera simulamos un submit de un formulario
+		wrapper.find('form').simulate('submit', { preventDefault() {} });
+
+		// lo que se espera es que la funcion setCategories no se ejecute o sea llamada
+		expect(setCategories).not.toHaveBeenCalled();
+	});
+	test('Debe de llamar el setCategories y limpiar la caja de texto', () => {
+		const value = 'hola mundo';
+		// 1. simular el inputChange
+		wrapper.find('input').simulate('change', { target: { value } });
+
+		// 2. simular el submit
+		wrapper.find('form').simulate('submit', { preventDefault() {} });
+
+		// 3. setCategories se debe de haber llamado
+		expect(setCategories).toHaveBeenCalled();
+		// la siguiente funcion me permite decirle que espero que se ejecute al menos 2 veces la funcion.
+		// expect(setCategories).toHaveBeenCalledTimes(2);
+		// la sguiente funcion me permite evaluar que lo que se espera sea una funcion en liugar de numeros o strings.
+		expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+
+		// 4. el valor del input debe de estar ''
+		expect(wrapper.find('input').prop('value')).toBe('');
 	});
 });
